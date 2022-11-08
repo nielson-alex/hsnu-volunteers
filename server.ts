7395029049d418f386d7ce31f5095f12c38e0bb7
Alex Nielson <alexnielson89@outlook.com>
2022-11-08 13:03:42 -0700

    Basic communication with mongoDB in place

--- server.ts
+++ server.ts
@@ -1,10 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
 const app = express();
 
+// Set up the default mongoose connection
+const mongoDB = `mongodb+srv://agnielson:HkUp'9L%3ArD&$ngj%60Q26h@cluster0.notmgra.mongodb.net/test`;
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const SomeModelSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  first_name: String,
+  last_name: String
+});
+
+// Compile model from schema
+const SomeModel = mongoose.model("hsnu.volunteers", SomeModelSchema);
+
+
+SomeModel.find()
+  .countDocuments()
+  .then(numProducts => {
+    let totalItems = numProducts;
+    return SomeModel.find();
+    // return Animal.find({ "_id": new mongoose.Types.ObjectId("60b2cb3847c44052a8042203") })
+    // .skip((20 - 1) * 20)
+    // .limit(20);
+  })
+  .then(volunteers => {
+    console.log("volunteers:", volunteers);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.post('/api/form', (req, res) => {
 
@@ -52,16 +94,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log('Server listening on port ' + PORT);
+  console.log('Server listening on port ' + PORT);
 });
 
 if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
-  }
+  // Exprees will serve up production assets
+  app.use(express.static('build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
